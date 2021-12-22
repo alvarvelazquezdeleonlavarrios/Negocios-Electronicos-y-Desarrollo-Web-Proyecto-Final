3646a95dfb4e5365956b4727aab4e3aeec778ceb
Alvar Velázquez de León Lavarrios <yatochbarum99@hotmail.com>
2021-12-22 17:35:26 -0600

    Videos, audios y otra información de videojuegos
    Descripción:
    - En la página videojuegos.html se agregaron nuevos campos relacionados
      a la información de un videojuego, como desarrollador(es) plataforma(s)
      y la cantidad que se desea adquirir.
    - Para cada uno de los campos, se aplicó un estilo en el tipo input para
      que la caja que contiene el dato sea transparente en la ventana.
    - El campo "cantidad" fue programado de tal manera que se validase un
      valor mayor o igual a 1, ya sea presionando los botones laterales o
      ingresando el número desde teclado.
    - Se agregaron enlaces de YouTube a los trailers de cada uno de los juegos.
    - La ventana que contiene el video del trailer es responsiva respecto al
      ancho de la ventana del navegador.
    - Se agregaron audios con duración de 30 segundos cada uno de una pista
      perteneciente al soundtrack de cada videojuego.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,4 @@
-var nombre, precio, imagen;
+var nombre, precio, imagen, desarrollador, plataforma, trailer, pista, soundtrack;
 
 function seleccionarVideojuego(indice) {
 	
@@ -8,30 +8,58 @@ function seleccionarVideojuego(indice) {
 			nombre = "Forza Horizon 5";
 			precio = 1499;
 			imagen = "Imágenes/Forza Horizon 5.jpg";
+			desarrollador = "Xbox Game Studios, Playground Games";
+			plataforma = "Xbox One, Xbox Series X|S";
+			trailer = "https://www.youtube.com/embed/FYH9n37B7Yw";
+
+			pista = "Pista no disponible";
 			break;
 
 		case 2:
 			nombre = "FIFA 22";
 			precio = 1499;
 			imagen = "Imágenes/FIFA 22.jpg";
+			desarrollador = "Electronic Arts";
+			plataforma = "PlayStation 4, PlayStation 5";
+			trailer = "https://www.youtube.com/embed/o1igaMv46SY";
+
+			pista = "Pista no disponible";
 			break;
 
 		case 3:
 			nombre = "Mario Kart 8 Deluxe";
 			precio = 1099;
 			imagen = "Imágenes/Mario Kart 8 Deluxe.jpg";
+			desarrollador = "Nintendo";
+			plataforma = "Nintendo Switch";
+			trailer = "https://www.youtube.com/embed/tKlRN2YpxRE";
+
+			pista = "Mario Kart 8 Deluxe OST - Start Screen";
+			soundtrack = "Audios/Mario Kart 8 Deluxe OST - Start Screen.ogg";
 			break;
 
 		case 4:
 			nombre = "Marvel's Spider-Man: Miles Morales";
 			precio = 1299;
 			imagen = "Imágenes/Spider-Man Miles Morales.jpg";
+			desarrollador = "Sony Interactive Entertainment, Insomniac Games";
+			plataforma = "PlayStation 4, PlayStation 5";
+			trailer = "https://www.youtube.com/embed/NTunTURbyUU";
+
+			pista = "Marvel's Spider-Man Miles Morales OST - New York's Only Spider-Man";
+			soundtrack = "Audios/Marvel's Spider-Man Miles Morales OST - New York's Only Spider-Man.ogg";
 			break;
 
 		case 5:
 			nombre = "Super Smash Bros. Ultimate";
 			precio = 1099;
 			imagen = "Imágenes/Super Smash Bros Ultimate.jpg";
+			desarrollador = "Nintendo, BANDAI NAMCO Studios";
+			plataforma = "Nintendo Switch";
+			trailer = "https://www.youtube.com/embed/hkTzHcHTcME";
+
+			pista = "Super Smash Bros Ultimate OST - Menu";
+			soundtrack = "Audios/Super Smash Bros Ultimate OST - Menu.ogg";
 			break;
 
 		/** Próximos lanzamientos **/
@@ -39,30 +67,59 @@ function seleccionarVideojuego(indice) {
 			nombre = "Halo Infinite";
 			precio = 1499;
 			imagen = "Imágenes/Halo Infinite.jpg";
+			desarrollador = "Xbox Game Studios, 343 Industries";
+			plataforma = "Xbox One, Xbox Series X|S";
+			trailer = "https://www.youtube.com/embed/PyMlV5_HRWk";
+
+			pista = "Halo Infinite OST - The Road";
+			soundtrack = "Audios/Halo Infinite OST - The Road.ogg";
 			break;
 
 		case 7:
 			nombre = "Battlefield 2042";
 			precio = 1359;
 			imagen = "Imágenes/Battlefield 2042.jpg";
+			desarrollador = "Electronic Arts, DICE";
+			plataforma = "Xbox One, Xbox Series X|S";
+			trailer = "https://www.youtube.com/embed/YWnFTdkrLq0";
+
+			pista = "Battlefield 2042 OST - Battlefield 2042";
+			soundtrack = "Audios/Battlefield 2042 OST - Battlefield 2042.ogg";
 			break;
 
 		case 8:
 			nombre = "Five Nights at Freddy's: Security Breach";
 			precio = 850;
 			imagen = "Imágenes/Five Nights at Freddy's Security Breach.jpg";
+			desarrollador = "Steel Wool Studios, Scottgames";
+			plataforma = "PlayStation 4, PlayStation 5";
+			trailer = "https://www.youtube.com/embed/BJ3cLVRWPFU";
+
+			pista = "Pista no disponible";
 			break;
 
 		case 9:
 			nombre = "Metroid Dread";
 			precio = 1299;
 			imagen = "Imágenes/Metroid Dread.jpg";
+			desarrollador = "Nintendo, MercurySteam";
+			plataforma = "Nintendo Switch";
+			trailer = "https://www.youtube.com/embed/V_XnbTayTH4";
+
+			pista = "Metroid Dread OST - Title Theme";
+			soundtrack = "Audios/Metroid Dread OST - Title Theme.ogg";
 			break;
 
 		case 10:
 			nombre = "Blue Reflection: Second Light";
 			precio = 1799;
 			imagen = "Imágenes/Blue Reflection Second Light.jpg";
+			desarrollador = "Koei Tecmo, Gust";
+			plataforma = "Nintendo Switch";
+			trailer = "https://www.youtube.com/embed/yYwBFVWOdZ4";
+
+			pista = "Blue Reflection Second Light OST - TIE";
+			soundtrack = "Audios/Blue Reflection Second Light OST - TIE.ogg";
 			break;
 
 		default:
@@ -74,4 +131,10 @@ function seleccionarVideojuego(indice) {
 	sessionStorage.setItem("nombreVideojuego", nombre);
 	sessionStorage.setItem("precioVideojuego", precio);
 	sessionStorage.setItem("imagenVideojuego", imagen);
+	sessionStorage.setItem("desarrolladorVideojuego", desarrollador);
+	sessionStorage.setItem("plataformaVideojuego", plataforma);
+	sessionStorage.setItem("trailerVideojuego", trailer);
+
+	sessionStorage.setItem("pista", pista);
+	sessionStorage.setItem("soundtrack", soundtrack);
 }
--- Scripts/videojuego.js
+++ Scripts/videojuego.js
@@ -1,11 +1,45 @@
-var nombre, precio, imagen;
+var nombre, precio, imagen, desarrollador, plataforma, cantidad = 1, trailer, pista, soundtrack;
 
 function cargarVideojuego() {
 	nombre = sessionStorage.getItem("nombreVideojuego");
 	precio = sessionStorage.getItem("precioVideojuego");
 	imagen = sessionStorage.getItem("imagenVideojuego");
+	desarrollador = sessionStorage.getItem("desarrolladorVideojuego");
+	plataforma = sessionStorage.getItem("plataformaVideojuego");
+	trailer = sessionStorage.getItem("trailerVideojuego");
+	pista = sessionStorage.getItem("pista");
+	soundtrack = sessionStorage.getItem("soundtrack");
 
 	document.getElementById("nombre").value = nombre;
 	document.getElementById("precio").value = precio;
 	document.getElementById("imagen").src = imagen;
+	document.getElementById("desarrollador").value = desarrollador;
+	document.getElementById("plataforma").value = plataforma;
+	document.getElementById("trailer").src = trailer;
+	document.getElementById("pista").value = pista;
+	document.getElementById("soundtrack").src = soundtrack;
+
+	var audio = document.getElementById("audio");
+	audio.load(); // Carga el nuevo archivo de audio seleccionado
+}
+
+function obtenerCantidad(){
+	cantidad = document.getElementById("cantidad").value;
+
+	if (cantidad <= 0){
+		cantidad = 1;
+		document.getElementById("cantidad").value = cantidad;
+	}
+}
+
+function aumntarCantidad(){
+	cantidad++;
+	document.getElementById("cantidad").value = cantidad;
+}
+
+function disminuirCantidad(){
+	if (cantidad > 1){
+		cantidad--;
+		document.getElementById("cantidad").value = cantidad;
+	}
 }
